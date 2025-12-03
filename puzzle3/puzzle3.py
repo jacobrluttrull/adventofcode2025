f1725a744639691549aff165783d3bc5a63a9d67
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-12-03 01:24:32 -0500

    finished part 2 of puzzle

--- puzzle3/puzzle3.py
+++ puzzle3/puzzle3.py
@@ -12,4 +12,35 @@ def find_highest_joltage():
             total += max_joltage
     return total
 
+
+def find_total_joltage_part_2():
+    total = 0
+    with open('input.txt') as file:
+        for line in file:
+            line = line.strip()
+            stack = []
+            digits_to_remove = len(line) - 12
+
+            for digit in line:
+                while stack and digit > stack[-1] and digits_to_remove > 0:
+                    stack.pop()
+                    digits_to_remove -= 1
+                stack.append(digit)
+
+            while digits_to_remove > 0:
+                stack.pop()
+                digits_to_remove -= 1
+
+            result = ''.join(stack)
+            total += int(result)
+
+    return total
+
+
+print(find_total_joltage_part_2())
+
+
+
+
 print(find_highest_joltage())
+print(find_total_joltage_part_2())
